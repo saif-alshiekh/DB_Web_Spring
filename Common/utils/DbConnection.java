09e07f844e899850fec502e74bdb5bf2c4bf4686
saifalghost7 <2110228@std.aum.edu.jo>
2024-09-15 00:37:00 +0300

    Setup Common directory with config and utility classes

---
+++ Common/utils/DbConnection.java
@@ -0,0 +1,23 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class DbConnection {
+    public static Connection getConnection() {
+        Connection conn = null;
+        try {
+            Properties props = new Properties();
+            props.load(new FileInputStream("Common/config/appConfig.properties"));
+
+            String url = props.getProperty("db.url");
+            String user = props.getProperty("db.user");
+            String password = props.getProperty("db.password");
+
+            conn = DriverManager.getConnection(url, user, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return conn;
+    }
+}
